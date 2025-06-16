3f91d3080ac4d50f6816406fcb99dc687586f2e2
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-06-16 10:22:17 +0700

    [IMPROVE] add response in health check

--- src/main/java/com/sarinah/peoplecounter/controller/HealthController.java
+++ src/main/java/com/sarinah/peoplecounter/controller/HealthController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
 
     @GetMapping("/check")
-    public String health() {
-        return "Success";
+    public Map<String, String> health() {
+        return Map.of("status", "Success");
     }
 
 }
