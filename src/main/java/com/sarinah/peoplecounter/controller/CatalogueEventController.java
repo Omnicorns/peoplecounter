779e5f85cbe88cfc109cedb406d2788154f1eaba
SARINAH\0852595 <aloysius.tanto@sarinah.co.id>
2025-12-06 21:23:17 +0700

    [IMPROVE]

--- src/main/java/com/sarinah/peoplecounter/controller/CatalogueEventController.java
+++ src/main/java/com/sarinah/peoplecounter/controller/CatalogueEventController.java
@@ -4,7 +4,7 @@ package com.sarinah.peoplecounter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class CatalogueEventController {
@@ -13,11 +13,11 @@ public class CatalogueEventController {
             Model model) {
 
         String pdfUrl;
-        pdfUrl = "/api/admin/users/pdf/643";
+        pdfUrl = "/api/admin/users/pdf/644";
 
 
 
         model.addAttribute("pdfUrl", pdfUrl);
-        return "flip"; // nama template viewer-mu
+        return "flip2"; // nama template viewer-mu
     }
 }
